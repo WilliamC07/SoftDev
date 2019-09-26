4cfa6bc85ad90f483cee65e9dc0538b16e3a7c16
JosephYusufov <jyusufov00@stuy.edu>
2019-09-26 09:16:42 -0400

    formatted code

--- 11_rename/app.py
+++ 11_rename/app.py
@@ -1,8 +1,8 @@
-# Alex Thompson (Thumb Thompson)
-# William Cao (Cow)
-# SoftDev1 pd2
-# K10 -- <Jinja Tuning/Flask&Jinja/Learning to use jinja>
-# 2019-09-23
+# Team Pizza Bagels
+# Joseph Yusufov
+# William Cao
+# Devin Lin
+# 2019-09-26
 
 from flask import Flask
 from flask import render_template
@@ -12,10 +12,13 @@ from flask import request
 app = Flask(__name__) #create instance of class Flask
 
 @app.route("/auth")
-def hello_world():
-    print(request)
-    print(request.args)
-    return render_template("index.html", login_info=request.args)
+def authentication(): 
+    """
+    This only accepts GET requests
+    """
+    print("\n" + "BODY OF REQUEST >>>" + str(request))
+    print("REQUEST ARGS >>>" + str(request.args) + "\n")
+    return render_template("index.html", login_info=request.args, method_type=request.method)
 
 
 @app.route('/')
