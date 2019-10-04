2b4a9d62d33c178c14421bba83775ea6ddb66681
William Cao <wcao00@stuy.edu>
2019-10-03 20:04:04 -0400

    continued work

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,33 +1,46 @@
 from flask import Flask
 from flask import session
+from flask import render_template
 from flask import redirect
 from flask import url_for
+from flask import request
 app = Flask(__name__)
 
 #def check_sess()
 
-def check_creds(user, pass):
+def check_creds(username, password):
     error = []
-    if (pass != 'pass'):
+    if (password != 'pass'):
         error.append('pass')
-    if (user != 'user'):
+    if (username != 'user'):
         error.append('user')
     return error
 
 @app.route("/")
 def home():
-    if check
-        return redirect('http://localhost:5000/login')
-    else:
-        return render_template('')
+    if (session.get('username') == None):
+        print ('no username')
+        return redirect(url_for("login"))
+    #else:
+        #cred_errors = check_creds(session.get('username'),session.get('password'))
+        #if (cred_errors == [])
+        #    return redirect(url_for("welcome"))
 
-@app.route('/login')
+
+@app.route('/login',methods = ['GET'])
 def login():
-    return "logged in successfully"
+    print(request.args('username'))
+    return render_template("login.html")
+
+
+
+@app.route("/welcome")
+def welcome():
+    return "You are logged in"
 
 @app.route('/bad_login')
 def bad_login():
-
+    pass
 
 if __name__ == "__main__":
     app.debug = True
