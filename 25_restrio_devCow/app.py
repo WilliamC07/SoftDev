36001a937b65055804affb52775405e0a44ac869
William Cao <wcao00@stuy.edu>
2019-11-14 09:01:47 -0500

    use urllib

--- 25_restrio_devCow/app.py
+++ 25_restrio_devCow/app.py
@@ -5,7 +5,7 @@
 
 from flask import Flask, render_template
 from datetime import datetime
-import urllib3
+import urllib
 import json
 
 app = Flask(__name__)
@@ -13,20 +13,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    url = "https://api.jikan.moe/v3/anime/36456"
-    http = urllib3.PoolManager()
-    response = http.request('GET', url)
-    data = json.loads(response.data)
+    url = urllib.request.urlopen("https://api.jikan.moe/v3/anime/36456")
+    response = url.read()
+    data = json.loads(response)
 
     return render_template("index.html", image_source=data["image_url"])
 
 
 @app.route("/anime")
 def anime():
-    url = "https://api.jikan.moe/v3/anime/36456"
-    http = urllib3.PoolManager()
-    response = http.request('GET', url)
-    data = json.loads(response.data)
+    url = urllib.request.urlopen("https://api.jikan.moe/v3/anime/36456")
+    response = url.read()
+    data = json.loads(response)
 
     return render_template("anime.html", image_source=data["image_url"], title_jap=data["title_japanese"],
                            title_english=data["title"], synopsis=data["synopsis"])
@@ -34,10 +32,9 @@ def anime():
 
 @app.route("/currency_exchange")
 def currency_exchange():
-    url = "https://api.exchangerate-api.com/v4/latest/USD"
-    http = urllib3.PoolManager()
-    response = http.request('GET', url)
-    data = json.loads(response.data)
+    url = urllib.request.urlopen("https://api.exchangerate-api.com/v4/latest/USD")
+    response = url.read()
+    data = json.loads(response)
     exchange = data["rates"].items()
     timestamp = datetime.utcfromtimestamp(data["time_last_updated"]).strftime('%Y-%m-%d %H:%M');
 
@@ -46,13 +43,13 @@ def currency_exchange():
 
 @app.route("/rick")
 def rick():
-    url = "https://rickandmortyapi.com/api/"
-    http = urllib3.PoolManager()
-    response = http.request('GET', url)
-    data = json.loads(response.data)
+    url = urllib.request.urlopen("https://rickandmortyapi.com/api/")
+    response = url.read()
+    data = json.loads(response)
 
-    response1 = http.request('GET', data['characters'])
-    data1 = json.loads(response1.data)
+    url1 = urllib.request.urlopen(data['characters'])
+    response1 = url1.read()
+    data1 = json.loads(response1)
     all_characters = data1['results']
     return render_template("rickMorty.html", rick=all_characters[0]['image'], name=all_characters[0]['name'],
                            status=all_characters[0]['status'], species=all_characters[0]['species'],
