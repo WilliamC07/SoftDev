6e9538060e94833b9fc4b07dcadec28d4b4d464f
William Cao <wcao00@stuy.edu>
2020-02-11 08:13:55 -0500

    finished k06

---
+++ 06_canvas/dotconn.js
@@ -0,0 +1,32 @@
+/*
+ * William Cao
+ * SoftDev1 pd1
+ * K06 -- Dot Dot Dot
+ * 2020-02-11
+ */
+const canvas = document.getElementById("playground");
+const context = canvas.getContext("2d");
+
+// can clear canvas
+document.getElementById("clear").addEventListener("click", () => {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+});
+
+// can draw dots and connect previous
+let previousPoint = undefined; // [x, y]
+canvas.addEventListener("click", (e) => {
+    const clickedPoint = [e.offsetX, e.offsetY];
+    // draw a point for where the user clicked
+    context.fillStyle = "blue";
+    context.fillRect(...clickedPoint, 5, 5);
+    // connect to previous point if one exists
+    if(previousPoint != undefined){
+        // there is a previous point
+        context.beginPath();
+        context.moveTo(...previousPoint);
+        context.lineTo(...clickedPoint);
+        context.stroke();
+    }
+    previousPoint = clickedPoint;
+});
