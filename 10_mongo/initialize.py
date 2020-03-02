8295c2c825e867efdb87c1be6d12499e0302dfa5
William Cao <wcao00@stuy.edu>
2020-03-01 23:19:50 -0500

    sterialized data

--- 10_mongo/initialize.py
+++ 10_mongo/initialize.py
@@ -8,6 +8,20 @@ database = client[TEAM_NAME]
 collection = database['meteoriteLandings']
 
 with open("data.json") as data_file:
-    collection.insert_many(json.load(data_file))
+    entries = json.load(data_file)
+
+# Turn mass into floating point for comparison
+cleaned_data = []
+for entry in entries:
+    # We need mass and classification
+    if "mass" not in entry.keys() or entry["recclass"] == "Unknown" or "geolocation" not in entry.keys():
+        print(f"Skipped id={entry['id']}")
+        continue
+
+    cleaned_data.append(entry)
+    entry["mass"] = float(entry["mass"])
+
+# insert to mongo
+collection.insert_many(cleaned_data)
 
 client.close()
