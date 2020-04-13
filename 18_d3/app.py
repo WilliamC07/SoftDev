22a8ff7f143ff5c5ef877c37b161cfc29397df03
William Cao <wcao00@stuy.edu>
2020-04-13 13:41:00 -0400

    can request the flask server for initial claims

--- 18_d3/app.py
+++ 18_d3/app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
-app = Flask(__name__) #create instance of class Flask
+import csv
+from flask import Flask, render_template, jsonify
+app = Flask(__name__)
 
 
 @app.route("/")
 def root():
     return render_template("index.html")
+
+
+@app.route("/data", methods={'GET'})
+def data():
+    """
+    GET the initial claims for the United States.
+
+    returns a JSON as an array of objects ({count: number, date: string})
+    Example return:
+    [
+        {count: "208000", date: "1967-01-07"},
+        {count: "207000", date: "1967-01-14"}
+    ]
+    """
+    payload = []
+    with open("data.csv") as csv_file:
+        reader = csv.reader(csv_file)
+        # skip the first row, which is the header row
+        next(reader)
+        for entry in reader:
+            payload.append({'date': entry[0], 'count': entry[1]})
+
+    return jsonify(payload)
+
 
 if __name__ == "__main__":
     app.debug = True
