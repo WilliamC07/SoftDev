ac52de170d6e09bf4547c8cd5f8a89c8a29b6709
William Cao <wcao00@stuy.edu>
2020-04-18 18:43:50 -0400

    can view data year by year

--- 18_d3/static/script.js
+++ 18_d3/static/script.js
@@ -7,6 +7,7 @@
 
 // References
 // https://bl.ocks.org/d3noob/402dd382a51a4f6eea487f9a35566de0
+let isFirstRender = true;
 
 // size of the graph
 const width = 800;
@@ -15,6 +16,12 @@ const height = 500;
 // animation purposes
 const animationDuration = 30; // in seconds
 const updatesPerSecond = 20;
+let partitionAmount;
+let currentIndex = 0;
+
+// type number
+let currentYearDisplayed;
+let maxYear;
 
 // the data to be shown to the user.
 let data;
@@ -37,7 +44,7 @@ const graph = d3.select("#container")
     .attr("width", width + 300)
     .attr("height", height + 300)
     .append("g")  // graph will be located inside of this (so we can see the y-axis description)
-    .attr("transform", "translate(100, 100)");
+    .attr("transform", "translate(100, 10)");
 
 /**
  * Data is formatted as:
@@ -70,13 +77,13 @@ function drawGraph(data){
         .call(d3.axisLeft(scaleY));
 }
 
-function updateGraph(data){
+function updateGraph(data, duration = 1000 / updatesPerSecond){
     // reference
     // https://bl.ocks.org/d3noob/7030f35b72de721622b8
 
     setScaling(data, scaleX, scaleY);
     const graph = d3.select("#container svg g").transition();
-    const duration = 1000 / updatesPerSecond;
+    d3.select("#displayed-year").html(`Looking from ${data[0].date.getFullYear()} to ${data[data.length - 1].date.getFullYear()}`);
     graph.select("path")
         .duration(duration)
         .attr("d", line(data));
@@ -101,8 +108,6 @@ function setScaling(data, scaleX, scaleY){
     scaleY.domain([0, maxInitialClaims]);
 }
 
-let partitionAmount;
-let currentIndex = 0;
 fetch("/data")
     .then((response) => response.json())
     .then((entries) => {
@@ -115,6 +120,8 @@ fetch("/data")
         }
         // update global variable
         data = entries;
+        maxYear = data[data.length - 1].date.getFullYear();
+        partitionAmount = Math.floor(data.length / (animationDuration * updatesPerSecond));
         d3.select("#render")
             .attr('disabled', null)
             .html("Render");
@@ -126,7 +133,8 @@ fetch("/data")
 
 d3.select("#start")
     .on("click", () => {
-        if(intervalID == null){
+        // do not animate if there is already an animation or the graph hasn't rendered yet
+        if(intervalID == null && !isFirstRender){
             intervalID = setInterval(() => {
                 // Need to use Math.min to check bounds
                 updateGraph(data.slice(0, Math.min(data.length - 1, currentIndex + partitionAmount)));
@@ -145,14 +153,54 @@ d3.select("#end")
             intervalID = null;
         }
 
+        // draw the first year
         currentIndex = 0;
-        partitionAmount = Math.floor(data.length / (animationDuration * updatesPerSecond));
-        updateGraph(data.slice(currentIndex, partitionAmount));
-        currentIndex += partitionAmount;
+        const firstYear =  data[0].date.getFullYear();
+        while(data[currentIndex].date.getFullYear() === firstYear){
+            currentIndex++;
+        }
+        d3.select("#displayed-year")
+            .html(`Looking from ${firstYear} to ${firstYear}`);
+
+        updateGraph(data.slice(0, currentIndex));
     });
 d3.select("#render")
     .on("click", () => {
-        partitionAmount = Math.floor(data.length / (animationDuration * updatesPerSecond));
-        drawGraph(data.slice(currentIndex, partitionAmount));
-        currentIndex += partitionAmount;
+        if(!isFirstRender) return;
+        isFirstRender = false;
+
+        // draw the first year
+        const firstYear =  data[0].date.getFullYear();
+        while(data[currentIndex].date.getFullYear() === firstYear){
+            currentIndex++;
+        }
+        d3.select("#displayed-year")
+            .html(`Looking from ${firstYear} to ${firstYear}`);
+        currentYearDisplayed = firstYear;
+        console.log("first render " + currentYearDisplayed);
+        drawGraph(data.slice(0, currentIndex + 1));
+    });
+d3.select("#next-year")
+    .on("click", () => {
+        if(isFirstRender){
+            alert("Please render first");
+            return;
+        }
+
+        if(intervalID !== null){
+            alert("Reset animation first");
+            return;
+        }
+        if(currentYearDisplayed + 1 > maxYear){
+            alert("No data for " + currentYearDisplayed + 1);
+            return;
+        }
+        currentYearDisplayed++;
+        currentIndex = 0;
+        while(data[currentIndex + 1].date.getFullYear() <= currentYearDisplayed && currentIndex + 1 < data.length - 1){
+            currentIndex++;
+        }
+        updateGraph(data.slice(0, currentIndex + 1), 2000);
+        d3.select("#displayed-year")
+            .html(`Looking from ${data[0].date.getFullYear()} to ${data[currentIndex].date.getFullYear()}`);
     });
